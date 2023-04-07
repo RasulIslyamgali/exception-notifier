cc87e1fb1b02195c66ec63a25a78c58be9030c2d
Islyamgali Rasul <r.islyamgali@trafficwave.kz>
2023-04-07 12:17:27 +0600

    added long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open('Readme.md') as f:
+    long_description = f.read()
+
+
 setup(
     name='pyexception-notifier',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='Exception notifier',
+    long_description=long_description,
     author='r.islyamgali',
     license='MIT',
     install_requires=[
