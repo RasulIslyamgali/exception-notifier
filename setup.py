c8ebed4799c0c14a7f113d69642c37f25cba0987
Islyamgali Rasul <r.islyamgali@trafficwave.kz>
2023-04-07 14:27:15 +0600

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 setup(
     name='pyexception-notifier',
     packages=find_packages(),
-    version='0.0.6',
+    version='0.0.7',
     description='Exception notifier',
     long_description=long_description,
     long_description_content_type='text/markdown',
